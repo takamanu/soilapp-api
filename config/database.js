e68a0c5e78d04a42d56be0fc874fb1a5ed008fd2
Takamanu <fauzanalivijsma@gmail.com>
2024-06-14 17:08:15 +0700

    change env

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 const Sequelize = require("sequelize");
 
 // configure this with your own parameters
+
 const database = new Sequelize({
   database: "soilapp",
   username: "root",
-  password: "",
+  password: "Kairobot123!",
   dialect: "mysql",
+  //   port:,
+  host: "34.172.160.94",
 });
 
 module.exports = database;
