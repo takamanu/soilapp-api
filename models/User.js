c9ece201d3098401b31c9bf1f0ad4397de38c220
Takamanu <fauzanalivijsma@gmail.com>
2024-06-14 17:49:01 +0700

    change model

--- models/User.js
+++ models/User.js
@@ -1,23 +1,20 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database');
+const Sequelize = require("sequelize");
+const db = require("../config/database");
 
 // creation of the User model
-const User = db.define('user', {
-
-    email: {
-        type: Sequelize.STRING,
-    },
-    password: {
-        type: Sequelize.STRING,
-    },
-    firstName :{
-        type: Sequelize.STRING
-    } ,
-    lastName : {
-        type: Sequelize.STRING
-    }
-   
+const User = db.define("user", {
+  email: {
+    type: Sequelize.STRING,
+  },
+  password: {
+    type: Sequelize.STRING,
+  },
+  first_name: {
+    type: Sequelize.STRING,
+  },
+  last_name: {
+    type: Sequelize.STRING,
+  },
 });
-
 
 module.exports = User;
